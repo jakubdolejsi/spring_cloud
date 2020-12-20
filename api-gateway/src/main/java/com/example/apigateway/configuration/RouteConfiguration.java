344b01dcaff1e1affafbfe45216af39a68a53d9d
Jakub <jakub.dolejsi3@gmail.com>
2020-12-20 11:55:13 +0100

    test

---
+++ api-gateway/src/main/java/com/example/apigateway/configuration/RouteConfiguration.java
@@ -0,0 +1,16 @@
+//package com.example.apigateway.configuration;
+//
+//import org.springframework.cloud.gateway.route.RouteLocator;
+//import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+//@Configuration
+//public class RouteConfiguration {
+//
+//    @Bean
+//    public RouteLocator routeLocator (RouteLocatorBuilder builder) {
+//        return null;
+//    }
+//
+//}
